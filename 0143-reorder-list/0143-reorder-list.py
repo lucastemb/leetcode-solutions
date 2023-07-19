5235576b36e6c31baf17a6cde6ac4d6fb2ab39aa
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-07-19 17:20:01 -0400

    Time: 106 ms (47.32%), Space: 26.1 MB (69.36%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,27 +8,41 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #idea: reverse one linked list and add by skipping one from each
         #time complexity: o(n)
-        slow, fast = head, head.next
-        while fast and fast.next: 
-            slow = slow.next 
-            fast = fast.next.next
         
-        second = slow.next
-        prev = slow.next = None
-        while second: 
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp 
-            
+        #idea: once fast.next is None slow will be at halfway point of LL
+        
+        #slice first half of linkedlist
+        slow, fast = head, head.next
+        while fast and fast.next: #fast will be None when odd and fast.next when even
+            slow=slow.next
+            fast=fast.next.next
+        
+        
+        
+        #reverse the second half of linkedlist 
+        second=slow.next
+        prev=slow.next=None
+        while second:
+            tmp=second.next
+            second.next=prev
+            prev=second
+            second=tmp
+        #second will be None since that pointer reached the end of the LL
+        #prev will now store the proper head of the LL
+        
+        #merge lists
         first, second = head, prev
         while second: 
             tmp1, tmp2 = first.next, second.next
-            first.next = second 
-            second.next = tmp1
+            first.next=second
+            second.next=tmp1
             first, second = tmp1, tmp2
+            
+            
+    
+        
+            
         
             
             

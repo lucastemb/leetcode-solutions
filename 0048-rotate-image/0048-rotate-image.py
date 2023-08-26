b5818713ea4b7e8b1b1c6f662e5ecc92d88503ef
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-08-26 16:04:10 -0400

    Time: 46 ms (54.53%), Space: 16.2 MB (84.11%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        #brute force to optimize
+        track=deepcopy(matrix)
+        res=[[0] * len(matrix) for i in range(len(matrix))]
+        for r in range(len(matrix)):
+            for c in range(len(matrix)):
+                matrix[r][c]=track[(len(matrix)-1-c)][r]
+        matrix=res
+
+        #time: O(N^2)
+        #space: O(N^2)

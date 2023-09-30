b5a9c6856db98a83683b18d4b3c23da81b1c58e2
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-09-30 17:37:04 -0400

    Time: 42 ms (64.64%), Space: 16.4 MB (76.98%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        '''
+        bc you have to find all possible permutations: do backtracking :)
+            (.)
+           /    \
+        (1)     (.)
+        /   \     /  \
+       (1,2)  (1)      (2)  (.)
+
+       [1,2,2]
+       []
+       [1]
+       [1,2]
+       [1,2,2]
+       [2]
+       [2,2]
+        '''
+        res=[]
+        subset=[]
+        def dfs(i):
+            if i >= len(nums):
+                if sorted(subset) not in res:
+                    res.append(sorted(subset.copy()))
+                return
+            
+            #if we take it
+            subset.append(nums[i])
+            dfs(i+1)
+
+            #don't take it
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
+
+        

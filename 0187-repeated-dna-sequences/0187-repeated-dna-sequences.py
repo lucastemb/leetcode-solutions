99495f688029905f38e7e23f6192cfe666b608fa
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-09-30 18:49:35 -0400

    Time: 86 ms (11.3%), Space: 30 MB (37.1%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -3,11 +3,12 @@ class Solution:
         sequences={}
         res=[]
         for i in range(len(s)-9):
-            print(i)
             if s[i:i+10] in sequences and s[i:i+10] not in res: 
                 res.append(s[i:i+10])
             sequences[s[i:i+10]]=1+sequences.get(s[i:i+10], 0)
         return res
+        #space: o(n)
+        #time: o(n)
             
             
         

794d0bc4c89786fb1828322b99dc267af3932bff
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-10-05 15:14:17 -0400

    Time: 133 ms (45.7%), Space: 19.7 MB (37.16%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,29 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, matrix: List[List[int]]) -> int:
-        biggestIslandArea = 0
+        biggestIslandArea=0
         ROWS=len(matrix)
         COLS=len(matrix[0])
-        visit=set()
+        visited=set()
 
         def dfs(r,c):
-            if r not in range(ROWS) or c not in range(COLS) or (r,c) in visit:
+            if (r,c) in visited or r not in range(ROWS) or c not in range(COLS) or matrix[r][c]==0:
                 return 0
-            if matrix[r][c]==0:
-                return 0
-            visit.add((r,c))
-            add=1
-            #up
-            add+=dfs(r-1,c)
-            #down
-            add+=dfs(r+1,c)
-            #right
-            add+=dfs(r,c+1)
-            #left
-            add+=dfs(r,c-1)
-            return add
-
+            visited.add((r,c))
+            return (1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1))
+            
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if (matrix[r][c]==1 and (r,c) not in visit):
+                if matrix[r][c] == 1 and (r,c) not in visited:
                     biggestIslandArea=max(dfs(r,c),biggestIslandArea)
         return biggestIslandArea

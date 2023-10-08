6f58a68d42fcb41cbf5500b85ae6539a847f33a7
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-10-08 11:43:19 -0400

    Time: 83 ms (46.58%), Space: 19.8 MB (87.2%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -4,17 +4,17 @@ class Solution:
         if they overlap, then you can increment the count by 1
         use heap if the min heap value is less than the start time of a meeting in the sorted array pop the min put the new one in 
         '''
-        if not intervals:
-            return 0
+
         h=[]
         intervals.sort(key=lambda x: x[0])
-        heapq.heappush(h,intervals[0][1])
+        heapq.heappush(h, intervals[0][1])
         for i in range(1, len(intervals)):
             if h[0] <= intervals[i][0]:
                 heapq.heappop(h)
-            heapq.heappush(h,intervals[i][1])
+            heapq.heappush(h, intervals[i][1])
         return len(h)
+
         
-        #space: O(N)
-        #time: O(1)
+        
+        
         

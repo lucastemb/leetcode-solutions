f811b3de523e154507bcc6458e1be549c284cb37
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-10-08 13:14:07 -0400

    Time: 1 ms (100%), Space: 60.4 MB (20.51%) - LeetHub

--- 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.java
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] findArray(int[] pref) {
-        int n = pref.length; 
-        for (int i = n -1; i > 0; i--){
-            pref[i]=pref[i] ^ pref[i-1];
+        int n = pref.length;
+        for (int i = n-1; i > 0; i--){
+            pref[i]= pref[i-1] ^ pref[i];
         }
         return pref;
     }

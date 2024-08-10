257b24243f2963fc9384dcb17398cd4239e07265
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-08-10 17:08:18 -0400

    Time: 2517 ms (5%), Space: 22.1 MB (5.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,20 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #have to incorporate elements of two sum which includes the current - sum 
-        nums = sorted(nums)
-        res=[] #store the result
-        for i,a in enumerate(nums):
-            #skip positives
-            if a > 0: 
-                break
-            if i > 0 and a == nums[i-1]:
-                continue
-            l, r = i+1, len(nums)-1
+    def threeSum(self, arr: List[int]) -> List[List[int]]:
+        triplets = []
+        inArr=set()
+        s = sorted(arr)
+        for i in range(len(arr)):
+            l=i+1
+            r=len(s)-1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0: 
-                    r-=1
-                elif threeSum < 0:
+                if s[l] + s[r] < abs(s[i]):
                     l+=1
-                else:
-                    res.append([a,nums[l],nums[r]])
-                    l+=1 
+                elif s[l] + s[r] > abs(s[i]):
                     r-=1
-                    while nums[l] == nums[l-1] and l < r:
-                        l+=1
-        return res
-
+                else:
+                    if ((s[i], s[l], s[r])) not in inArr:
+                        triplets.append([s[i], s[l], s[r]])
+                        inArr.add((s[i], s[l], s[r]))
+                    l+=1
+        return triplets
+        

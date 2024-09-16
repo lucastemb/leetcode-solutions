3a8c7f2c5028d02f5c04be628f7c116eaad83df0
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-09-16 13:49:15 -0400

    Time: 954 ms (31.83%), Space: 54.9 MB (64.34%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,27 +1,15 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
         #create adjacency list 
-        adj_list = collections.defaultdict(list)
         indegrees = collections.defaultdict(int)
         for u,v in edges:
-            adj_list[u].append(v) #it's directed
             indegrees[v]+=1
-        #create dfs
-        def dfs(node, visited):
-            if node not in visited:
-                visited.add(node)
-                for edge in adj_list[node]:
-                    dfs(edge, visited)
 
         #start creating the array
-        visited=set()
         res=[] #resulting array
-        for vertex in list(adj_list):
-            if vertex not in visited:
-                if indegrees[vertex] == 0:
-                    res.append(vertex)
-                dfs(vertex, visited)
-                visited.add(vertex)
+        for i in range(n):
+            if indegrees[i] < 1:
+                res.append(i)
         return res
 
 

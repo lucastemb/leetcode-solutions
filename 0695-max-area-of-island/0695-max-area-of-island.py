821a00f8066ff4593cf36a12dd8a8e01dc1f3177
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-09-16 14:10:40 -0400

    Time: 119 ms (29.03%), Space: 18.6 MB (9.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,19 +1,16 @@
 class Solution:
-    def maxAreaOfIsland(self, matrix: List[List[int]]) -> int:
-        biggestIslandArea=0
-        ROWS=len(matrix)
-        COLS=len(matrix[0])
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         visited=set()
+        max_area=0
 
         def dfs(r,c):
-            if (r,c) in visited or r not in range(ROWS) or c not in range(COLS) or matrix[r][c]==0:
+            if (r,c) in visited or r not in range(len(grid)) or c not in range(len(grid[0])) or grid[r][c] == 0:
                 return 0
             visited.add((r,c))
-            return (1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1))
-            
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if matrix[r][c] == 1 and (r,c) not in visited:
-                    biggestIslandArea=max(dfs(r,c),biggestIslandArea)
-        return biggestIslandArea
+            return 1+dfs(r,c+1)+dfs(r,c-1)+dfs(r+1,c)+dfs(r-1,c)
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1 and (r,c) not in visited:
+                    max_area=max(dfs(r,c), max_area)
+        return max_area
